799d4a66f2c6ac223cfc9d4eb95c9b27aee38807
Anzelah <jepkoechanzellah@gmail.com>
2024-06-07 12:17:20 +0300

    tried using the v1 UserHandler authentication to see if it works. It doesn't

--- bot.py
+++ bot.py
@@ -2,16 +2,20 @@ import tweepy
 import config
 
 
-client = tweepy.Client(
-    consumer_key=config.consumer_key,
-    consumer_secret=config.consumer_secret,
-    access_token=config.access_token,
-    access_token_secret=config.access_token_secret
-)
-client.create_tweet(text='Testing tweepy.')
+#client = tweepy.Client(
+#    consumer_key=config.consumer_key,
+#    consumer_secret=config.consumer_secret,
+#    access_token=config.access_token,
+#    access_token_secret=config.access_token_secret
+#)
+#client.create_tweet(text='Testing tweepy.')
+auth = tweepy.OAuth1UserHandler(config.consumer_key, config.consumer_secret, config.access_token, config.access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
-#try:
-#	api.update_status("testing tweepy")
-#	print('Tweet posted successfully')
-#except tweepy.TweepyException as e:
-#	print('An error occured: {}' .format(e))
+print(api.verify_credentials().screen_name)
+
+try:
+	api.update_status("testing tweepy with user handler authentication")
+	print('Tweet posted successfully')
+except tweepy.TweepyException as e:
+	print('An error occured: {}' .format(e))
