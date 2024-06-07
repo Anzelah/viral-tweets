5a950b74736e60c7c6c0f7f246ca458ed93003e6
Anzelah <jepkoechanzellah@gmail.com>
2024-06-07 14:09:14 +0300

    attach a text to an image for upload

--- bot.py
+++ bot.py
@@ -1,21 +1,25 @@
 import tweepy
 import config
 
+#v2 authentication for creating tweets
+client = tweepy.Client(
+    consumer_key=config.consumer_key,
+    consumer_secret=config.consumer_secret,
+    access_token=config.access_token,
+    access_token_secret=config.access_token_secret,
+    wait_on_rate_limit=True
+)
 
-#client = tweepy.Client(
-#    consumer_key=config.consumer_key,
-#    consumer_secret=config.consumer_secret,
-#    access_token=config.access_token,
-#    access_token_secret=config.access_token_secret
-#)
-#client.create_tweet(text='Testing tweepy.')
+#v1 authentication for uploading media
 auth = tweepy.OAuth1UserHandler(config.consumer_key, config.consumer_secret, config.access_token, config.access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
+#post an image with attached text
 print(api.verify_credentials().screen_name)
-
 try:
-	api.update_status("testing tweepy with user handler authentication")
-	print('Tweet posted successfully')
+	media = api.media_upload(filename='images/mot.jpg')
+	print(media)
+	media_id = media.media_id
+	client.create_tweet(text='Morning motivation', media_ids=[media_id])
 except tweepy.TweepyException as e:
 	print('An error occured: {}' .format(e))
