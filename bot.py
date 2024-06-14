e13298617299f258cda61aa5898688d12f45552e
Anzelah <jepkoechanzellah@gmail.com>
2024-06-14 19:30:30 +0300

    finally merged the tweeting functionality. Can say the bot is done. Only need to add the scheduler

--- bot.py
+++ bot.py
@@ -2,43 +2,47 @@ import tweepy
 import config
 import random
 
-#v2 authentication for creating tweets
-# client = tweepy.Client(
-    # consumer_key=config.consumer_key,
-    # consumer_secret=config.consumer_secret,
-    # access_token=config.access_token,
-    # access_token_secret=config.access_token_secret,
-    # wait_on_rate_limit=True
-# )
-
-#v1 authentication for uploading media
-# auth = tweepy.OAuth1UserHandler(config.consumer_key, config.consumer_secret, config.access_token, config.access_token_secret)
-# api = tweepy.API(auth, wait_on_rate_limit=True)
-
-#post an image with attached text
-# print(api.verify_credentials().screen_name)
-# try:
-	# media = api.media_upload(filename='images/mot.jpg')
-	# print(media)
-	# media_id = media.media_id_string
-	# client.create_tweet(text='Morning  motivation', media_ids=[media_id])
-# except tweepy.TweepyException as e:
-	# print('An error occured: {}' .format(e))
-
-
 # File manipulation
-with open('lyrics.txt', 'r', encoding="utf-8") as f:
-    content = f.read().split('"')
+def find_lyrics(file_path):
+	"""A function that retrieves random lyrics from our text file. 
+	It takes the file/path to file as the argument.
+	"""
+	with open(file_path, 'r', encoding="utf-8") as f:
+		content = f.read().split('"')
 
-    #remove extra whitespaces between lyrics
-    content_list = [i.strip() for i in content]
+        #remove extra whitespaces between lyrics
+        lyrics_list = [i.strip() for i in content]
 
-    #remove empty strings
-    while('' in content_list):
-	    content_list.remove('')
+        #remove empty strings
+	    while('' in lyrics_list):
+	        lyrics_list.remove('')
 
-    #choose Random lyrics
-    a = random.choice(content_list)
-    print(a)
+        #choose random lyrics   
+        lyrics = random.choice(lyrics_list)
+	return lyrics
+
+#BOT
+#v2 authentication for creating tweets
+client = tweepy.Client(
+    consumer_key=config.consumer_key,
+    consumer_secret=config.consumer_secret,
+    access_token=config.access_token,
+    access_token_secret=config.access_token_secret,
+    wait_on_rate_limit=True
+)
+
+
+tweeted_lyrics = []
+#post the tweets using v2 client
+try:
+	text = find_lyrics('lyrics.txt')
+	print(text)
+	client.create_tweet(text=text)
+	print('Tweet created succesfully!')
+
+	tweeted_lyrics.append(text)
+except tweepy.TweepyException as e:
+	print('An error occured: {}' .format(e))
+
 
 	
